3d41d67517d4cd01a2a3067e174f83c844b257ac
Andrew Cooper <andrew.cooper3@citrix.com>
2012-08-21 19:16:49 +0100

    Further licence fixup.

--- include/Xen.h
+++ include/Xen.h
@@ -1,18 +1,18 @@
 /*
  *  This file is part of the Xen Crashdump Analyser.
  *
- *  Foobar is free software: you can redistribute it and/or modify
+ *  Xen Crashdump Analyser is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  Foobar is distributed in the hope that it will be useful,
+ *  Xen Crashdump Analyser is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Xen Crashdump Analyser.  If not, see <http://www.gnu.org/licenses/>.
  *
  *  Copyright (c) 2011,2012 Citrix Inc.
  */
