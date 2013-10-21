cba5b1ad6ca6d1ebec95d8e3c03bd6dc3f387229
Andrew J. Bennieston <andrew.bennieston@citrix.com>
2013-10-21 17:14:12 +0100

    CP-6715: Add CoreInfo class to wrap vmcoreinfo data.
    The CoreInfo class wraps pointers to data from VMCOREINFO ELF notes,
    deleting it when the object goes out of scope. Ownership may be
    transferred from one CoreInfo object to another. Only one CoreInfo
    object will own a resource at any time. CoreInfo objects can be told to
    destroy the data they own at any time, allowing them to be used as local
    variables or member variables, benefitting from RAII while still having
    fine control over the lifetime of the owned resource.

---
+++ include/coreinfo.hpp
@@ -0,0 +1,108 @@
+/*
+ *  This file is part of the Xen Crashdump Analyser.
+ *
+ *  The Xen Crashdump Analyser is free software: you can redistribute
+ *  it and/or modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation, either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  The Xen Crashdump Analyser is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the Xen Crashdump Analyser.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2013 Citrix Inc.
+ */
+
+#ifndef __COREINFO_HPP__
+#define __COREINFO_HPP__
+
+/**
+ * @file include/coreinfo.hpp
+ * @author Andrew J. Bennieston
+ */
+
+#include <cstring>
+
+/**
+ * Wrapper class for storing vmcoreinfo strings,
+ * supporting RAII semantics.
+ */
+class CoreInfo {
+    private:
+        /**
+         * Copy constructor.
+         * Copy constructor is private to prevent unwanted copying.
+         */
+        CoreInfo(const CoreInfo&);
+
+        /**
+         * Copy assignment operator.
+         * Copy assignment operator is private to prevent unwanted
+         * copying.
+         */
+        CoreInfo& operator=(const CoreInfo&);
+
+        /// ELF note name, with trailing '\0' to behave like a C-string
+        char * name;
+
+        /// ELF note data, with trailing '\0' to behave like a C-string
+        char * data;
+
+    public:
+        /// Constructor
+        CoreInfo();
+
+        /**
+         * Constructor
+         * @param note_name ELF note name
+         * @param name_size length of name (excluding NULL terminator)
+         * @param note_data ELF note data
+         * @param data_size length of data (excluding NULL terminator)
+         * @throws std::bad_alloc in the case of insufficient memory
+         */
+        CoreInfo(const char * note_name, const size_t name_size,
+                 const char * note_data, const size_t data_size);
+
+        /// Destructor
+        ~CoreInfo();
+
+        /*
+         * Destroy the data owned by this CoreInfo object
+         */
+        void destroy();
+
+        /**
+         * Retrieve vmcoreinfo ELF note name.
+         * @return C-string of name, or NULL.
+         */
+        const char * vmcoreinfoName() const { return name; }
+
+        /**
+         * Retrieve vmcoreinfo ELF note data.
+         * @return C-string of newline-separated key=value pairs, or NULL.
+         */
+        const char * vmcoreinfoData() const { return data; }
+
+        /**
+         * Transfer ownership of vmcoreinfo into this object from another.
+         * @param other The object to transfer ownership from
+         */
+        void transferOwnershipFrom(CoreInfo& other);
+};
+
+#endif
+
+/*
+ * Local variables:
+ * mode: C++
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
---
+++ src/coreinfo.cpp
@@ -0,0 +1,79 @@
+/*
+ *  This file is part of the Xen Crashdump Analyser.
+ *
+ *  The Xen Crashdump Analyser is free software: you can redistribute
+ *  it and/or modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation, either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  The Xen Crashdump Analyser is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the Xen Crashdump Analyser.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2013 Citrix Inc.
+ */
+
+/**
+ * @file src/coreinfo.cpp
+ * @author Andrew J. Bennieston
+ */
+
+#include "coreinfo.hpp"
+#include "util/macros.hpp"
+
+#include <cstring>
+
+CoreInfo::CoreInfo()
+ : name(NULL), data(NULL)
+{}
+
+CoreInfo::CoreInfo(const char * note_name, const size_t name_size,
+                   const char * note_data, const size_t data_size)
+ : name(NULL), data(NULL)
+{
+    name = new char[name_size + 1];
+    memcpy(name, note_name, name_size);
+    name[name_size] = '\0';
+
+    data = new char[data_size + 1];
+    memcpy(data, note_data, data_size);
+    name[data_size] = '\0';
+}
+
+CoreInfo::~CoreInfo()
+{
+    this->destroy();
+}
+
+void CoreInfo::destroy()
+{
+    SAFE_DELETE_ARRAY(name);
+    SAFE_DELETE_ARRAY(data);
+}
+
+void CoreInfo::transferOwnershipFrom(CoreInfo& other)
+{
+    // Clean up anything already owned by this instance
+    this->destroy();
+
+    // Assume single-threaded!
+    this->name = other.name;
+    other.name = NULL;
+    this->data = other.data;
+    other.data = NULL;
+}
+
+/*
+ * Local variables:
+ * mode: C++
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
